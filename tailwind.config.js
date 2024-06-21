2df7e497e4a32a6059fa0c850aba9a3a9d34431b
lamidimorufat <lamidimorufat0@gmail.com>
2024-06-21 18:51:16 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        orange: '#FFA500',
+        veryDarkOrange: '#FF7A01',
+        lightGreen: '#60B86D',
+        deepGreen: '#489E5C',
+        veryLightGreen: '#CADDD3',
+        lightBlue: '#2CAAC2',
+        deepBlue: '#0000FF',
+        veryDarkBlue: '#051C3B',
+        grey: '#31405F',
+        white: '#FFFFFF',
+        black: '#000000',
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
